df82f50bc2367278424f029d8a48500dddfd1778
Nicolas Sorin <nicolas.sorin@insa-lyon.fr>
2015-12-08 17:59:52 +0100

    TP Reseau chat : Debut d'interface graphique

--- Reseau/chat/client/Output.java
+++ Reseau/chat/client/Output.java
@@ -2,9 +2,37 @@ package chat.client;
 
 import chat.client.protocol.OutputItf;
 
-public class Output implements OutputItf {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
-	public Output() {}
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Output implements OutputItf extends JFrame {
+
+	private JButton sendButton;
+	private JTextField textField;
+	private JPanel controlPanel;
+	private JLabel msgLabel;
+	private GridLayout layout;
+
+	public Output() {
+		super();
+		this.setTitle("34Chat");
+		sendButton = new JButton("Send");
+		textField = new JTextField();
+		controlPanel = new JPanel();
+		controlPanel.setLayout(new GridLayout(1, 2));
+		controlPanel.add(textField);
+		controlPanel.add(sendButton);
+		msgLabel = new JLabel();
+		this.setLayout(new GridLayout(2, 1));
+		this.add(msgLabel);
+		this.add(controlPanel);
+	}
 
 	public void display(String notif) {
 		System.out.println(notif);
