543b088a4cd20692acf9d1c5f55d40c168b0c3de
Olivice <olivier.vicente95@gmail.com>
2016-04-06 13:24:07 +0200

    TP MultiTache : Avancement de la tâche Entree

--- tp-multitache/Entree.cpp
+++ tp-multitache/Entree.cpp
@@ -18,6 +18,8 @@
 #include <sys/shm.h>
 #include <map>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 //------------------------------------------------------ Include personnel
 #include "Entree.h"
 #include "Donnees.h"
@@ -41,10 +43,27 @@ static map<pid_t,Voiture> voituriersEnService;
 //------------------------------------------------------ Fonctions privées
 static void destruction(int noSignal)
 {
-  //Fermeture du canal de communication
-  close(descCanalSimu);
+  // On annule l'impact des signaux SIGCHLD au cas ou un
+  // voiturier mourrait dans l'intervalle de temps
+  struct sigaction actionChild;
+  actionChild.sa_handler = SIG_IGN;
+  sigemptyset(&actionChild.sa_mask);
+  actionChild.sa_flags = 0;
+  sigaction(SIGUSR2, &actionChild, NULL);
 
-  //Suicide de la tâche
+  // Demande de suicide à tous les voituriers en service
+  for(map<pid_t,Voiture>::iterator it=voituriersEnService.begin(); it!=voituriersEnService.end(); it++)
+  {
+    kill(it->first,SIGUSR2);
+  }
+  // Attente des réponses des voituriers
+  for(map<pid_t,Voiture>::iterator it=voituriersEnService.begin(); it!=voituriersEnService.end(); it++)
+  {
+    waitpid(it->first,NULL,0);
+  }
+  // Fermeture du canal de communication
+  close(descCanalSimu);
+  // Suicide de la tâche
   exit(0);
 } //----- fin de destruction
 
@@ -105,14 +124,62 @@ static void moteur()
     // Attachement à la mémoire partagée d'état du parking
     StructMemEtat* memEtat = (StructMemEtat*) shmat(memEtatId, NULL, 0);
     // Lecture de la mémoire partagée d'état du parking
-
-    // Détachement de la mémoire partagée d'état du parking
-    shmdt(memEtat);
-
-    // Création du voiturier en charge de la voiture
-    pid_t voiturier = GarerVoiture(barriere);
-    // Ajout du voiturier dans la map des voituriers en service
-    voituriersEnService.insert(pair<pid_t, Voiture>(voiturier,voiture));
+    if(memEtat->nbPlacesPrises<NB_PLACES)
+    {
+      // Modification du nombre de places prises
+      memEtat->nbPlacesPrises++;
+      // Détachement de la mémoire partagée d'état du parking
+      shmdt(memEtat);
+      // Libération du sémaphore d'état
+      while(semop(semEtatId, &liberation, 1)==-1 && errno==EINTR);
+      // Création du voiturier en charge de la voiture
+      pid_t voiturier = GarerVoiture(barriere);
+      // Ajout du voiturier dans la map des voituriers en service
+      voituriersEnService.insert(pair<pid_t, Voiture>(voiturier,voiture));
+    }
+    else
+    {
+      // Détachement de la mémoire partagée d'état du parking
+      shmdt(memEtat);
+      // Libération du sémaphore d'état
+      while(semop(semEtatId, &liberation, 1)==-1 && errno==EINTR);
+      // Attente du sémaphore de requêtes à 0 = Mémoire Partagée libre
+      while(semop(semRequeteId, &attente, 1)==-1 && errno==EINTR);
+      // Réservation du sémaphore de requêtes
+      while(semop(semRequeteId, &reservation, 1)==-1 && errno==EINTR);
+      // Attachement à la mémoire partagée des requêtes
+      StructMemRequete* memReq = (StructMemRequete*) shmat(memRequeteId,NULL,0);
+      // Ajout de la requête
+      RequetePlace req;
+      req.barriere = barriere;
+      req.voiture = voiture;
+      req.arrivee = 3;
+      memReq->requetes[barriere-1] = req;
+      // Détachement de la mémoire partagée des requêtes
+      shmdt(memReq);
+      // Libération du sémaphore de requêtes
+      while(semop(semRequeteId, &liberation, 1)==-1 && errno==EINTR);
+      //Affichage de la requête
+      AfficherRequete(req.barriere, req.voiture.type, req.arrivee);
+      // Attente du sémaphore de la sortie
+      while(semop(semRequeteId, &attente, 1)==-1 && errno==EINTR);
+      // Attente du sémaphore d'état à 0 = Mémoire Partagée libre
+      while(semop(semEtatId, &attente, 1)==-1 && errno==EINTR);
+      // Réservation du sémaphore d'état
+      while(semop(semEtatId, &reservation, 1)==-1 && errno==EINTR);
+      // Attachement à la mémoire partagée d'état du parking
+      StructMemEtat* memEtat = (StructMemEtat*) shmat(memEtatId, NULL, 0);
+      // Modification du nombre de places prises
+      memEtat->nbPlacesPrises++;
+      // Détachement de la mémoire partagée d'état du parking
+      shmdt(memEtat);
+      // Libération du sémaphore d'état
+      while(semop(semEtatId, &liberation, 1)==-1 && errno==EINTR);
+      // Création du voiturier en charge de la voiture
+      pid_t voiturier = GarerVoiture(barriere);
+      // Ajout du voiturier dans la map des voituriers en service
+      voituriersEnService.insert(pair<pid_t, Voiture>(voiturier,voiture));
+    }
   }
   else
   {
